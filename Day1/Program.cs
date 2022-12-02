122ef23bd4098d0ff44ed9740c9296c568bb5c86
Simon Phillips <simon@icuk.net>
2022-12-02 16:15:55 +0000

    Finish Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Read file
+string input = File.ReadAllText("input.txt");
+
+var elves = input.Split("\n\n");
+
+List<int> elvesCalories = new List<int>();
+foreach (string elf in elves) {
+    int elfCalories = elf.Split("\n").Select(x => Convert.ToInt32(x)).Sum();
+    elvesCalories.Add(elfCalories);
+}
+Console.WriteLine("Elf with the most calories has: " + elvesCalories.Max());
+Console.WriteLine("Top 3 elves have: " + elvesCalories.OrderByDescending(x => x).Take(3).Sum());
+
+Console.ReadKey();
+
